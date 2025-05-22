58bdfd99a352c39702ac34db17e42d3527d412f1
zakgc <zakgriffithcrowe@gmail.com>
2025-05-22 18:39:02 +0100

    Add JSON validation to validateRequestBody service

--- app/services/validateRequestBody.test.ts
+++ app/services/validateRequestBody.test.ts
@@ -2,6 +2,14 @@ import { describe, expect, test } from "vitest";
 import { validateRequestBody } from "./validateRequestBody";
 
 describe('validateRequestBody', () => {
+  test('returns 400 if requestBody is invalid JSON', () => {
+    let requestBody = {
+      array: [ 16, 23, ]
+    }
+
+    expect(validateRequestBody(requestBody)).toBe(400)
+  })
+
   test('returns 400 if character count is less than 5', () => {
     let text = 'Tiny'
     let requestBody = {
--- app/services/validateRequestBody.ts
+++ app/services/validateRequestBody.ts
@@ -1,6 +1,10 @@
 import { characterCount } from "./characterCount"
 
 export const validateRequestBody = (body: any) => {
+  if (isInvalidJSON(body)) {
+    return 400
+  }
+
   let textCharacterCount = characterCount(body.text)
 
   if (textCharacterCount < 5 || textCharacterCount > 300) {
@@ -8,4 +12,14 @@ export const validateRequestBody = (body: any) => {
   }
 
   return body.text
+}
+
+const isInvalidJSON = (body: any): boolean => {
+  try {
+    JSON.parse(body)
+  } catch (error) {
+    return true
+  }
+
+  return false
 }
