d4d4eb7183d8cc289490fd5ccb55bc6f16fb5e03
zakgc <zakgriffithcrowe@gmail.com>
2025-05-22 18:08:36 +0100

    Fix typo in mostCommonLetter test

--- app/services/mostCommonLetter.test.ts
+++ app/services/mostCommonLetter.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest";
 import { mostCommonLetter } from "./mostCommonLetter";
 
 describe('mostCommonLetter', () => {
-  test('returns the amount of words in string', () => {
+  test('returns the most common letter in the string', () => {
     const text = 'This is a test string with lots and lots of letters'
 
     expect(mostCommonLetter(text)).toBe('t')
