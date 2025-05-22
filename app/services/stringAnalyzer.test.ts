5545435754e0e3e7eee4e7e9ee1663f1041b1b6b
zakgc <zakgriffithcrowe@gmail.com>
2025-05-22 17:52:35 +0100

    Add stringAnalyzer service

---
+++ app/services/stringAnalyzer.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it, test } from "vitest";
+import { stringAnalyzer } from "./stringAnalyzer";
+
+describe('stringAnalyzer', () => {
+  test('returns all the analysis of a string', () => {
+    const text = 'This is a test string'
+    let analysis = stringAnalyzer(text)
+
+    expect(analysis.wordCount).toBe(0)
+    expect(analysis.characterCount).toBe(0)
+    expect(analysis.lineCount).toBe(0)
+    expect(analysis.longestWordLength).toBe(0)
+    expect(analysis.mostCommonLetter).toBe(0)
+  })
+})
---
+++ app/services/stringAnalyzer.ts
@@ -0,0 +1,9 @@
+export const stringAnalyzer = (text: string) => {
+  return {
+    wordCount: 0,
+    characterCount: 0,
+    lineCount: 0,
+    longestWordLength: 0,
+    mostCommonLetter: 0
+  }
+}
