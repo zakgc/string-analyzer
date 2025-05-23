91d9601c94c7e99720227ca520f36824925962a9
zakgc <zakgriffithcrowe@gmail.com>
2025-05-23 11:52:20 +0100

    index can read body

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 export const handler = async (event: any) => {
+  const body = JSON.parse(event.body)
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Fresh start because of Gremlins" })
+    body: JSON.stringify({ message: "Fresh start because of Gremlins", body: body})
   };
 };
